58f8951643cea7487b46ae1003bc9ee671b66b56
Jezer Mejía <amyuki4@icloud.com>
2022-11-13 19:23:14 -0600

    Open MainActivity from Login

--- app/src/main/java/com/jezerm/healthzone/LoginActivity.kt
+++ app/src/main/java/com/jezerm/healthzone/LoginActivity.kt
@@ -1,11 +1,32 @@
 package com.jezerm.healthzone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jezerm.healthzone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                this.putExtra("logged_in", true)
+                this.putExtra("doctor_mode", false)
+            }
+            startActivity(intent)
+        }
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                this.putExtra("logged_in", true)
+                this.putExtra("doctor_mode", true)
+            }
+            startActivity(intent)
+        }
     }
 }
