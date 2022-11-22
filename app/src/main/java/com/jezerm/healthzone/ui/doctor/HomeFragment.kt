e3ab1212466d0cd92e6525ef0a597847c6ac815f
Armando <amezal01@hotmail.com>
2022-11-22 00:34:23 -0600

    getting user data inside fragment

--- app/src/main/java/com/jezerm/healthzone/ui/doctor/HomeFragment.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/doctor/HomeFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jezerm.healthzone.MainActivity
 import com.jezerm.healthzone.R
+import com.jezerm.healthzone.data.AppDatabase
 import com.jezerm.healthzone.databinding.FragmentHomeDoctorBinding
+import com.jezerm.healthzone.entities.User
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeDoctorBinding
-
+    private lateinit var doctor: User
+    private lateinit var patients: List<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             //param1 = it.getString(ARG_PARAM1)
+        }
+        doctor = MainActivity.user
+        val db = AppDatabase.getInstance(requireContext())
+        val userDao = db.userDao()
+        runBlocking {
+            launch {
+                patients = userDao.getDoctorsPatients(doctor.id)
+            }
         }
     }
 
