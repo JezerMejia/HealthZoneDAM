23ee3931eb876ac9e6d50f4afc2588fa1b4a5950
Jezer Mejía <amyuki4@icloud.com>
2022-11-21 19:30:49 -0600

    Reorder Appointment entity. Apply ZonedDateTime typeConverter to Appointment entity.

--- app/src/main/java/com/jezerm/healthzone/entities/Appointment.kt
+++ app/src/main/java/com/jezerm/healthzone/entities/Appointment.kt
@@ -1,15 +1,15 @@
 package com.jezerm.healthzone.entities
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.Ignore
-import androidx.room.PrimaryKey
+import androidx.room.*
+import com.jezerm.healthzone.utils.DateTime
+import java.time.ZonedDateTime
 
 @Entity
+@TypeConverters(DateTime::class)
 class Appointment(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "date") val date: String,
-    @ColumnInfo(name = "doctor_id") val doctorId: String,
-    @ColumnInfo(name = "patient_id") val patientId: String,
+    @ColumnInfo(name = "date") val date: ZonedDateTime,
     @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "doctor_id") val doctorId: Int,
+    @ColumnInfo(name = "patient_id") val patientId: Int,
 )
