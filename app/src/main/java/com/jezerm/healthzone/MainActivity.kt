e072d8caad22a7eba1e1ce9cea4e43f6cfec8824
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-21 23:47:40 -0600

    Added a settings menu

--- app/src/main/java/com/jezerm/healthzone/MainActivity.kt
+++ app/src/main/java/com/jezerm/healthzone/MainActivity.kt
@@ -58,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home,
                 R.id.navigation_appointments,
                 R.id.navigation_maps,
+                R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
