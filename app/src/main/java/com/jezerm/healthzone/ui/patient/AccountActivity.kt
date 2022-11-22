099e491ce464f026c13786adcf900292bc2f6a14
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-22 15:11:16 -0600

    Added a user ui

--- app/src/main/java/com/jezerm/healthzone/ui/patient/AccountActivity.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/patient/AccountActivity.kt
@@ -1,10 +1,16 @@
 package com.jezerm.healthzone.ui.patient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import com.jezerm.healthzone.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.jezerm.healthzone.databinding.ActivityAccountPatientBinding
+import com.jezerm.healthzone.ui.patient.AccountFragments.FirstFragment
+import com.jezerm.healthzone.ui.patient.AccountFragments.SecondFragment
 
 class AccountActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAccountPatientBinding
@@ -12,10 +18,9 @@ class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityAccountPatientBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        start()
         setSupportActionBar(binding.toolbar)
 
         supportActionBar?.apply {
@@ -26,6 +31,46 @@ class AccountActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+    }
+
+    fun start() {
+        val adapter = TabsFragmentAdapter(
+            supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+        )
+        adapter.addItem(FirstFragment(), "Detalles")
+        adapter.addItem(SecondFragment(), "Citas")
+
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = adapter
+
+        val tabLayout: TabLayout = binding.tabs
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    class TabsFragmentAdapter(fm: FragmentManager, behavior: Int) :
+        FragmentPagerAdapter(fm, behavior) {
+
+        private val listaFragment: MutableList<Fragment> = ArrayList()
+        private val titleList: MutableList<String> = ArrayList()
+
+        fun addItem(fragment: Fragment, titulo: String) {
+            listaFragment.add(fragment)
+            titleList.add(titulo)
+        }
+
+        override fun getCount(): Int {
+            return listaFragment.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return listaFragment[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/jezerm/healthzone/ui/patient/AccountFragments/FirstFragment.kt
@@ -0,0 +1,18 @@
+package com.jezerm.healthzone.ui.patient.AccountFragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jezerm.healthzone.R
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.account_patient_fragment_1, container, false)
+    }
+}
