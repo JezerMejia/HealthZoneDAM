a0d59d445835eee5993f5f23c8002c0381597f2a
Jezer Mejía <amyuki4@icloud.com>
2022-11-23 01:01:41 -0600

    Added insertDoctor and insertPatient methods

--- app/src/main/java/com/jezerm/healthzone/entities/User.kt
+++ app/src/main/java/com/jezerm/healthzone/entities/User.kt
@@ -11,24 +11,24 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 class User (
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "username") val username: String,
-    @ColumnInfo(name = "password") val password: String,
-    @ColumnInfo(name = "first_name") val firstName: String,
-    @ColumnInfo(name = "middle_name") val middleName: String? = null,
-    @ColumnInfo(name = "last_name") val lastName: String,
-    @ColumnInfo(name = "telephone") val telephone: String? = null,
-    @ColumnInfo(name = "is_doctor") val isDoctor: Boolean = true,
+    @ColumnInfo(name = "username") var username: String,
+    @ColumnInfo(name = "password") var password: String,
+    @ColumnInfo(name = "first_name") var firstName: String,
+    @ColumnInfo(name = "middle_name") var middleName: String? = null,
+    @ColumnInfo(name = "last_name") var lastName: String,
+    @ColumnInfo(name = "telephone") var telephone: String? = null,
+    @ColumnInfo(name = "is_doctor") var isDoctor: Boolean = true,
 
     // Doctor only
-    @ColumnInfo(name = "hospital_id") val hospitalId: Int? = null,
-    @ColumnInfo(name = "specialty") val specialty: String? = null,
+    @ColumnInfo(name = "hospital_id") var hospitalId: Int? = null,
+    @ColumnInfo(name = "specialty") var specialty: String? = null,
 
     // Patient only
-    @ColumnInfo(name = "age") val age: Int? = null,
-    @ColumnInfo(name = "weight") val weight: Float? = null,
-    @ColumnInfo(name = "height") val height: Float? = null,
-    @ColumnInfo(name = "sex") val sex: Boolean? = null,
-    @ColumnInfo(name = "conditions") val conditions: String? = null,
+    @ColumnInfo(name = "age") var age: Int? = null,
+    @ColumnInfo(name = "weight") var weight: Float? = null,
+    @ColumnInfo(name = "height") var height: Float? = null,
+    @ColumnInfo(name = "sex") var sex: Boolean? = null,
+    @ColumnInfo(name = "conditions") var conditions: String? = null,
 
     ) : Parcelable {
     @Ignore var fullName: String = ""
