597f62f0640143ddf4e67fd02d58368d19791ac7
Jezer Mejía <amyuki4@icloud.com>
2022-11-23 01:02:30 -0600

    Some tests

--- app/src/main/java/com/jezerm/healthzone/ui/register/RegisterPatientFragment.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/register/RegisterPatientFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jezerm.healthzone.data.AppDatabase
+import com.jezerm.healthzone.data.UserDAO
 import com.jezerm.healthzone.databinding.FragmentRegisterPatientBinding
+import com.jezerm.healthzone.entities.User
 
 class RegisterPatientFragment : Fragment() {
     private lateinit var binding: FragmentRegisterPatientBinding
+    private lateinit var userDao: UserDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             //param1 = it.getString(ARG_PARAM1)
         }
+        val db = AppDatabase.getInstance(requireContext())
+        userDao = db.userDao()
     }
 
     override fun onCreateView(
@@ -23,5 +30,44 @@ class RegisterPatientFragment : Fragment() {
     ): View? {
         binding = FragmentRegisterPatientBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            addPatient()
+        }
+        binding.btnCancel.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun addPatient() {
+        val username = binding.etUsername.text.toString()
+        val password = binding.etPassword.text.toString()
+        val firstName = binding.etFirstName.text.toString()
+        val middleName = binding.etMiddleName.text.toString()
+        val lastName = binding.etLastname.text.toString()
+        val cellphone = binding.etCellphone.text.toString()
+
+        if (username.isBlank()) {
+            binding.etUsername.error = "Campo requerido"
+            return
+        }
+        if (password.isBlank()) {
+            binding.etPassword.error = "Campo requerido"
+            return
+        }
+        if (firstName.isBlank()) {
+            binding.etFirstName.error = "Campo requerido"
+            return
+        }
+        if (lastName.isBlank()) {
+            binding.etLastname.error = "Campo requerido"
+            return
+        }
+
+        val user = User(-1, username, password, firstName, middleName, lastName, cellphone)
     }
 }
