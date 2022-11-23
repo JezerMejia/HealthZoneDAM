380f4047b82b11560bfd7139a10ffa426af269a3
Jezer Mejía <amyuki4@icloud.com>
2022-11-23 09:53:21 -0600

    Implemented addDoctor method to insert new doctors in database

--- app/src/main/java/com/jezerm/healthzone/ui/register/RegisterMedicoFragment.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/register/RegisterMedicoFragment.kt
@@ -5,16 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jezerm.healthzone.MainActivity.Companion.user
+import com.jezerm.healthzone.data.AppDatabase
+import com.jezerm.healthzone.data.UserDAO
 import com.jezerm.healthzone.databinding.FragmentRegisterMedicoBinding
+import com.jezerm.healthzone.entities.User
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class RegisterMedicoFragment: Fragment() {
     private lateinit var binding: FragmentRegisterMedicoBinding
+    private lateinit var userDao: UserDAO
+    private lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            //param1 = it.getString(ARG_PARAM1)
+            user = it.getParcelable("user")!!
         }
+        val db = AppDatabase.getInstance(requireContext())
+        userDao = db.userDao()
     }
 
     override fun onCreateView(
@@ -23,5 +34,54 @@ class RegisterMedicoFragment: Fragment() {
     ): View? {
         binding = FragmentRegisterMedicoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etUsername.setText(user.username)
+        binding.btnRegister.setOnClickListener {
+            addDoctor()
+        }
+    }
+
+    private fun addDoctor() {
+        val email = binding.etEmail.text.toString()
+        val specialty = binding.etSpecialty.text.toString()
+        val hospital = binding.etWorkplace.text.toString()
+
+        if (email.isBlank()) {
+            binding.etEmail.error = "Campo requerido"
+            return
+        }
+        if (specialty.isBlank()) {
+            binding.etSpecialty.error = "Campo requerido"
+            return
+        }
+        if (hospital.isBlank()) {
+            binding.etWorkplace.error = "Campo requerido"
+            return
+        }
+
+        user.specialty = specialty
+        user.hospitalId = 1
+
+        runBlocking {
+            launch {
+                userDao.insertDoctor(
+                    user.username,
+                    user.password,
+                    user.firstName,
+                    user.middleName,
+                    user.lastName,
+                    user.telephone,
+                    user.hospitalId!!,
+                    user.specialty!!
+                )
+
+                val action = RegisterMedicoFragmentDirections.actionRegisterDoctorToLogin()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
