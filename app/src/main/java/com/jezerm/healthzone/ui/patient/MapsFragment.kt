4285332125b5c6fa5189245970b5016beb410375
Jezer Mejía <amyuki4@icloud.com>
2022-11-27 01:20:41 -0600

    Implemented/copied Maps from Google demos to request location permissions

--- app/src/main/java/com/jezerm/healthzone/ui/patient/MapsFragment.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/patient/MapsFragment.kt
@@ -1,32 +1,37 @@
 package com.jezerm.healthzone.ui.patient
 
-import android.Manifest
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import androidx.fragment.app.Fragment
-
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
-
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.jezerm.healthzone.R
+import com.jezerm.healthzone.ui.patient.maps.PermissionUtils
+import com.jezerm.healthzone.ui.patient.maps.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
+import com.jezerm.healthzone.ui.patient.maps.PermissionUtils.isPermissionGranted
 
-class MapsFragment : Fragment() {
 
+class MapsFragment : Fragment(), ActivityCompat.OnRequestPermissionsResultCallback,
+    OnMyLocationButtonClickListener,
+    OnMyLocationClickListener {
     private lateinit var map: GoogleMap
-    companion object{
-        const val REQUEST_CODE_LOCATION = 0
-    }
+    private var permissionDenied = false
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -38,14 +43,20 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val UCA = LatLng(12.1254835,-86.2708279)
+
+        map = googleMap
+        map.setOnMyLocationButtonClickListener(this);
+        map.setOnMyLocationClickListener(this);
+        enableMyLocation()
+
+        val UCA = LatLng(12.1254835, -86.2708279)
         googleMap.addMarker(MarkerOptions().position(UCA).title("UCA, managua, Nicaragua"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(UCA))
-        googleMap.uiSettings.isZoomControlsEnabled = true
         googleMap.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(UCA,18f),4000,null
+            CameraUpdateFactory.newLatLngZoom(UCA, 18f), 4000, null
         )
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -56,44 +67,121 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        enableLocation()
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
 
-    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
-        requireContext() ,Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
 
-    private fun enableLocation(){
-        if(!::map.isInitialized) return
-        if(isLocationPermissionGranted()){
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             map.isMyLocationEnabled = true
-        }else{
-            requestLocationPermission()
+            return
         }
+
+        // 2. If if a permission rationale dialog should be shown
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                ACCESS_COARSE_LOCATION
+            )
+        ) {
+            PermissionUtils.RationaleDialog.newInstance(
+                LOCATION_PERMISSION_REQUEST_CODE, true
+            ).show(parentFragmentManager, "dialog")
+            return
+        }
+
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                ACCESS_FINE_LOCATION,
+                ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
     }
 
-    private fun requestLocationPermission(){
-        if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
-            Toast.makeText(getActivity(),"Acepta permisos", Toast.LENGTH_SHORT).show()
-        }else{
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE_LOCATION)
-        }
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(requireContext(), "MyLocation button clicked", Toast.LENGTH_SHORT)
+            .show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(requireContext(), "Current location:\n$location", Toast.LENGTH_LONG)
+            .show()
     }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
-        when(requestCode){
-            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                map.isMyLocationEnabled = true
-            }else{
-                Toast.makeText(getActivity(),"Acepta los permisos", Toast.LENGTH_SHORT).show()
-            }
-            else -> {}
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                ACCESS_COARSE_LOCATION
+            )
+        ) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        newInstance(true).show(parentFragmentManager, "dialog")
+    }
+
+    companion object {
+        /**
+         * Request code for location permission request.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
 }
