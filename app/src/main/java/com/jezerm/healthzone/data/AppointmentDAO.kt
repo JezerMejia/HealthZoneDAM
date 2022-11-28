3d2b296d191120af3f8c8ee46d47aef9fcb74d48
Jezer Mejía <amyuki4@icloud.com>
2022-11-28 02:48:28 -0600

    Added getAppointments of patient and doctor queries

--- app/src/main/java/com/jezerm/healthzone/data/AppointmentDAO.kt
+++ app/src/main/java/com/jezerm/healthzone/data/AppointmentDAO.kt
@@ -1,18 +1,32 @@
 package com.jezerm.healthzone.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.jezerm.healthzone.entities.Appointment
+import com.jezerm.healthzone.entities.User
 
 @Dao
 interface AppointmentDAO {
     @Query("SELECT * FROM Appointment")
     suspend fun getAll(): List<Appointment>
 
-    @Query("INSERT INTO Appointment" +
-            "(date, description, doctor_id, patient_id) VALUES" +
-            "('2022-11-25 19:00 America/Managua', 'aaaaaa', 2, 1)")
+    @Query(
+        "INSERT INTO Appointment" +
+                "(date, description, doctor_id, patient_id) VALUES" +
+                "('2022-11-25 19:00 America/Managua', 'aaaaaa', 2, 1)"
+    )
     suspend fun insertTestAppointment()
+
+    @Query("SELECT * FROM Appointment WHERE patient_id = :patientId")
+    suspend fun getAppointmentsOfPatient(patientId: Int): List<Appointment>
+
+    suspend fun getAppointmentsOfPatient(patient: User): List<Appointment> {
+        return getAppointmentsOfPatient(patient.id)
+    }
+
+    @Query("SELECT * FROM Appointment WHERE doctor_id = :doctorId")
+    suspend fun getAppointmentsOfDoctor(doctorId: Int): List<Appointment>
+
+    suspend fun getAppointmentsOfDoctor(doctor: User): List<Appointment> {
+        return getAppointmentsOfDoctor(doctor.id)
+    }
 }
