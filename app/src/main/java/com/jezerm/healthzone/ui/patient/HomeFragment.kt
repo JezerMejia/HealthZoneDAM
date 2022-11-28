674d8af82f7150f31d308648855021f751c61615
Jezer Mejía <amyuki4@icloud.com>
2022-11-28 00:55:30 -0600

    Testing calendarView decorators

--- app/src/main/java/com/jezerm/healthzone/ui/patient/HomeFragment.kt
+++ app/src/main/java/com/jezerm/healthzone/ui/patient/HomeFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.transition.MaterialFadeThrough
 import com.jezerm.healthzone.MainActivity
 import com.jezerm.healthzone.R
 import com.jezerm.healthzone.databinding.FragmentHomePatientBinding
+import com.jezerm.healthzone.ui.patient.home.EventDecorator
+import com.jezerm.healthzone.ui.patient.home.OneDayDecorator
+import com.prolificinteractive.materialcalendarview.CalendarDay
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomePatientBinding
@@ -29,6 +33,31 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomePatientBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.calendarView.addDecorator(
+            EventDecorator(
+                arrayListOf(
+                    CalendarDay.today(),
+                    CalendarDay.from(2022, 11, 28),
+                    CalendarDay.from(2022, 11, 30),
+                    CalendarDay.from(2022, 11, 15),
+                )
+            )
+        )
+        binding.calendarView.addDecorator(
+            OneDayDecorator()
+        )
+        binding.calendarView.setOnDateChangedListener { widget, date, selected ->
+            Toast.makeText(
+                requireContext(),
+                "${date.year}-${date.month}-${date.day}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onStart() {
